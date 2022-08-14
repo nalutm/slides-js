dbf313969d03ccc6c710f3f3736a21e33dd9bf7c
nalutm <nayara.luiza21@gmail.com>
2022-08-14 18:20:27 -0300

    :construction: Distance calculation when events occur

--- assets/js/_slide.js
+++ assets/js/_slide.js
@@ -2,21 +2,33 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0 , movement: 0 }
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform =  `translate3d(${distX}px, 0, 0)`
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
   }
 
   onStart(e) {
     e.preventDefault();
-    console.log(this);
+    this.dist.startX = e.clientX;
     this.wrapper.addEventListener('mousemove', this.onMove);
   }
 
   onMove(e) {
-    console.log('Moveu!');
+    const finalPosition = this.updatePosition(e.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(e) {
-    console.log('Acabou!');
     this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   addSlideEvents() {
