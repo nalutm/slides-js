efbc24145b647897023f8aedb0666859beee9591
nalutm <nayara.luiza21@gmail.com>
2022-08-15 06:09:55 -0300

    Added slide pagination

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,4 @@ import { SlideNav } from "./_slide.js";
 const slide = new SlideNav('.slides', '.slides-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
-console.log(slide);
+slide.addControl()
